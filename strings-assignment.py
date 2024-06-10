20ad3c2ff13fc5c3801ea4e38deccf3ce7d42700
Kalyanpavan11 <167405068+Kalyanpavan11@users.noreply.github.com>
2024-06-10 10:49:11 +0530

    Create strings-assignment.py

---
+++ strings-assignment.py
@@ -0,0 +1,19 @@
+"""
+Write a Program to retrieve character 'X' using positive indexing
+from "Semantic segmentation of Lung Cancer from Chest X-Ray" string
+"""
+s="Semantic segmentation of Lung Cancer from Chest X-Ray"
+print(s[48])
+
+'''
+Write a Program to retrieve the 'L' character from "I have to Love Python Programming" string
+using negative/reverse indexing
+'''
+s="I have to Love Python Programming"
+print(s[-23])
+
+'''
+Can prove that python string is immutable with the program
+'''
+s="python"
+s[0]='3'
